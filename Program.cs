5059148de95350ab76e3c693324c9946daaefec2
LisKH <115477615+LisKH@users.noreply.github.com>
2023-11-02 21:12:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿//стр 13 табл 1. № 2
+//Console.WriteLine("Введите значение x");
+//double x = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("Введите значение y");
+//double y = Convert.ToDouble(Console.ReadLine());
+//if (x * x + y * y >= 9 && x * x + y * y <= 25 && x >= 0)
+//{
+//    Console.WriteLine("Находится");
+//}
+//else
+//{
+//    Console.WriteLine("Не находится");
+//}
+//стр 13 табл 1. № 3
+//Console.WriteLine("Введите значение x");
+//double x = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("Введите значение y");
+//double y = Convert.ToDouble(Console.ReadLine());
+//if (y <= 4 && y >= 0 && x >= -2 && x <= 4 || y >= -3 && y <= 0 && x >= -4 && x <= 5)
+//{
+//    Console.WriteLine("Находится");
+//}
+//else
+//{
+//    Console.WriteLine("Не находится");
+//}
+//стр 13 табл 1. № 4
+//Console.WriteLine("Введите значение x");
+//double x = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("Введите значение y");
+//double y = Convert.ToDouble(Console.ReadLine());
+//if (y >= 5 - x && y >= x - 3 && y <= 3 + x && y <= -x + 11)
+//{
+//    Console.WriteLine("Находится");
+//}
+//else
+//{
+//    Console.WriteLine("Не находится");
+//}
